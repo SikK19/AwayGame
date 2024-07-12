e1eeef62f5e8a0df699a1dcb821d09bdd1e25a31
sven.o.troschke <sven.o.troschke@student.fh-kiel.de>
2024-07-12 23:08:54 +0200

    improved bird flying to cursor with stopping in air

--- bird.gd
+++ bird.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 250.0
+@export var JUMP_VELOCITY = -400.0
 
 @onready var sprite = $Sprite
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var mouse_position = null;
+var direction_flying = null;
 
 
 func _physics_process(delta):
@@ -18,8 +19,15 @@ func _physics_process(delta):
 		
 	mouse_position = get_global_mouse_position()
 	if Input.is_action_pressed("fly_to_cursor"):
-		var direction_flying = (mouse_position - position).normalized()
+		direction_flying = (mouse_position - position).normalized()
 		velocity = (direction_flying * SPEED)
+		if direction_flying.x > 0:
+			sprite.flip_h = true
+		if direction_flying.x < 0:
+			sprite.flip_h = false
+			
+	if Input.is_action_just_released("fly_to_cursor"):
+		velocity.x = 0
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():

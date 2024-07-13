2c881e3a6b18fd5a37ef65324114a0714290b263
Georg-Neugebauer <georg.neugebauer@googlemail.com>
2024-07-13 16:29:31 +0200

    added movable ladders

---
+++ Objects/ladder.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var start_position:Vector2= Vector2(0,0)
+@export var end_position:Vector2 = Vector2(0,1000)
+
+func _ready():
+	var animation = $AnimationPlayer.get_animation("ladder drop")
+
+	var track = animation.find_track("ladder:position", Animation.TYPE_VALUE)
+	var last_key = animation.track_get_key_count(track) - 1
+
+	animation.track_set_key_value(track, 0, start_position)
+	animation.track_set_key_value(track, last_key, end_position)
+
+func activate():
+	print("hello")
+	$AnimationPlayer.play("ladder drop")

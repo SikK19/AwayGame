8d2ec0ddc5ee0709f5ede7bf70648f7c967eb40a
Georg-Neugebauer <georg.neugebauer@googlemail.com>
2024-07-13 23:13:49 +0200

    checked and fixed all interactables

--- Objects/rain.gd
+++ Objects/rain.gd
@@ -2,10 +2,9 @@ extends StaticBody2D
 
 @export var active:bool = true
 
-
 func _ready():
-	$CollisionShape2D.disabled = active
-	#$rain.visible = active
+	#$CollisionShape2D.disabled = not active
+	$rain.visible = active
 	
 	$rain.size = $CollisionShape2D.shape.size
 	$rain.position = $CollisionShape2D.position - $CollisionShape2D.shape.size / 2

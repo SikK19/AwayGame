763c4fe060208285fde32fda061a929c3801dbec
SikK19 <asiofy247@gmail.com>
2024-07-13 19:12:39 +0200

    new assets by katja for bird with animation

--- Characters/Bird/bird.gd
+++ Characters/Bird/bird.gd
@@ -4,8 +4,9 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY = -400.0
 @export var human:Node2D 
 
-@onready var sprite_standing = $Sprite
+@onready var sprite_idle = $Idle
 @onready var sprite_flying = $Flying
+@onready var sprite_picking = $Picking
 
 var has_key = false
 
@@ -16,20 +17,35 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var mouse_position = null
 var direction_flying = null
 
+func _ready():
+	sprite_idle.visible = false
+	sprite_flying.visible = false
+	sprite_picking.visible = false
+	if is_on_floor():
+		sprite_idle.play("default")
+		sprite_idle.visible = true
+		sprite_flying.visible = false
+	else:
+		sprite_flying.play("default")
+		sprite_flying.visible = true
+		sprite_idle.visible = false
+
 func _physics_process(delta):
 	if is_on_character:
+		sprite_flying.stop()
 		position.x = human.position.x
 		position.y = human.position.y - 60
 	
 	if is_on_floor() && not Input.is_action_pressed("fly_to_cursor"):
-		sprite_standing.visible = true
+		sprite_idle.play("default")
+		sprite_idle.visible = true
 		sprite_flying.visible = false
 		velocity.x = 0
 	
 	# Add the gravity.
 	if not is_on_floor() && not is_on_character:
 		velocity.y += gravity * delta
-		sprite_standing.visible = false
+		sprite_idle.visible = false
 		sprite_flying.visible = true
 		sprite_flying.play("default")
 		
@@ -39,11 +55,11 @@ func _physics_process(delta):
 		velocity = (direction_flying * SPEED)
 		if abs(mouse_position.x - position.x) > 10:
 			if direction_flying.x > 0.2:
-				sprite_standing.flip_h = true
-				sprite_flying.flip_h = true
-			if direction_flying.x < -0.1:
-				sprite_standing.flip_h = false
+				sprite_idle.flip_h = false
 				sprite_flying.flip_h = false
+			if direction_flying.x < -0.1:
+				sprite_idle.flip_h = true
+				sprite_flying.flip_h = true
 				
 	
 		
@@ -72,7 +88,7 @@ func _physics_process(delta):
 	
 func land_on_character():
 	sprite_flying.visible = false
-	sprite_standing.visible = true
+	sprite_idle.visible = true
 	is_on_character = true
 	human.is_bird_on_human = true
 	

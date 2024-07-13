4d1ffaeef892ae4de60c8973e500122add7b0c74
SikK19 <asiofy247@gmail.com>
2024-07-13 21:04:30 +0200

    revert to working state

--- Characters/Human Character/CameraBirdHuman.gd
+++ Characters/Human Character/CameraBirdHuman.gd
@@ -1,9 +1,10 @@
 extends Camera2D
 
-@export var bird:Node2D
-@export var human:Node2D
+#@export var bird:Node2D
+#@export var human:Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x = (bird.position.x + human.position.x)/2
-	position.y = (bird.position.y + human.position.y)/2
+	pass
+	#position.x = (bird.position.x + human.position.x)/2
+	#position.y = (bird.position.y + human.position.y)/2

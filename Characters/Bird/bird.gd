3d650146032e5394cba95381729d0c13384345c1
SikK19 <asiofy247@gmail.com>
2024-07-13 21:53:34 +0200

    Character animations jumping and climbing

--- Characters/Bird/bird.gd
+++ Characters/Bird/bird.gd
@@ -60,10 +60,8 @@ func _physics_process(delta):
 			if direction_flying.x < -0.1:
 				sprite_idle.flip_h = true
 				sprite_flying.flip_h = true
-				
-	
 		
-	if Input.is_action_just_released("interact") && $interactrange.has_overlapping_areas():
+	if Input.is_action_just_pressed("interact") && $interactrange.has_overlapping_areas():
 		$interactrange.get_overlapping_areas()[0].interact(self)
 		
 	if Input.is_action_just_released("fly_to_cursor") && $bird_close_to_human.has_overlapping_areas():
--- Characters/Human Character/CameraBirdHuman.gd
+++ Characters/Human Character/CameraBirdHuman.gd
@@ -1,10 +1,9 @@
 extends Camera2D
 
-#@export var bird:Node2D
-#@export var human:Node2D
+@export var bird:Node2D
+@export var human:Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-	#position.x = (bird.position.x + human.position.x)/2
-	#position.y = (bird.position.y + human.position.y)/2
+	global_position.x = (bird.global_position.x + human.global_position.x)/2
+	global_position.y = (bird.global_position.y + human.global_position.y)/2

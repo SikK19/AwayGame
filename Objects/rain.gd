c532805700ff4563c7a6f76a4563c1664b8b6b0d
Georg-Neugebauer <georg.neugebauer@googlemail.com>
2024-07-13 21:47:59 +0200

    added rain

--- Objects/rain.gd
+++ Objects/rain.gd
@@ -1,23 +1,15 @@
-extends Area2D
+extends StaticBody2D
 
-@export var area_left:Area2D
-@export var area_right:Area2D
-@export var bird:Node2D
+@export var active:bool = true
 
-var wind_force = 0
 
 func _ready():
-	$AnimatedSprite2D.play("default")
+	$CollisionShape2D.disabled = active
+	$rain.visible = active
+	
+	$rain.size = $CollisionShape2D.shape.size
+	$rain.position = $CollisionShape2D.position - $CollisionShape2D.shape.size / 2
+	
+	
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if not area_left.has_overlapping_areas() && not area_right.has_overlapping_areas():
-		wind_force = 0
-	if area_left.has_overlapping_areas():
-		wind_force += 10
-		print("wind_force: " + str(wind_force))
-		bird.velocity.x += wind_force
-	if area_right.has_overlapping_areas():
-		wind_force += 10
-		print("wind_force: " + str(wind_force))
-		bird.velocity.x -= wind_force

ee0829f650c3bca043cd7d243b9e545261b2bf97
SikK19 <asiofy247@gmail.com>
2024-07-14 11:11:55 +0200

    Window shattering sound + logic

--- Objects/window.gd
+++ Objects/window.gd
@@ -2,7 +2,9 @@ extends Area2D
 
 func interact(_rock):
 	$window.visible = false
+	$ShatterGlass.play()
 	$window_broken.visible = true
+	
 	
 	$StaticBody2D.queue_free()
 	set_collision_layer_value(4, false)
